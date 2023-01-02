a1bc98385edb62967645d1797a7b5c90c6fcbac9
SiGGFedCJKP <baannokhooksg@gmail.com>
2023-01-02 11:32:46 +0700

    SiGG-FinancialTech-Service-CounterParty-Quater-Controllers-HomeController

---
+++ HomeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GM.Service.CounterParty.Controllers
+{
+    public class HomeController : Controller  
+    {
+      public string Index() => "Repo Counter Party API";   
+    }
+}
